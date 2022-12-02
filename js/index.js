ab108d74eb9cc283a994a79785ca3d6a99c6e477
tbfrobinson <tbfrobinson@gmail.com>
2022-12-02 14:02:10 -0800

    added some text that appears

--- js/index.js
+++ js/index.js
@@ -44,7 +44,7 @@ class Character {
 }
 // create character
 const guy = new Character( {
-    x: 0,
+    x: 1,
     y: 0,
 })
 
@@ -57,15 +57,16 @@ class Platform {
     }
     append() {
         c.fillStyle = 'grey'
-        c. fillRect(this.position.x, this.position.y, 70, 10)
+        c.fillRect(this.position.x, this.position.y, 70, 10)
     }
 }
 
 const plat = new Platform( {
-    x: 100,
+    x: 155,
     y: 600
 })
 
+// adding text
 class Text {
     constructor(x, y, text, size){
         this.x = x 
@@ -80,13 +81,43 @@ class Text {
     }
 
 }
+
+class Object {
+    constructor(x, y, width, height, fillStyle){
+        this.x = x
+        this.y = y
+        this.width = width
+        this.height = height
+        this.fillStyle = fillStyle
+    }
+    append() {
+        c.fillStyle = this.fillStyle
+        c.fillRect(this.x, this.y, this.width, this.height)
+    }
+}
+
+
+
+const crow = new Object (323, 570, 20, 20, 'black')
+const tree = new Object (300, 450, 65, 300, 'brown')
+
+
+// first screen text
 function instructions() {
     const arrowKeys = new Text(150, 300, 'arrow keys to move left and right', 25)
     const jump = new Text(240, 330, 'and space to jump', 22)
-    
+    const jumpHere = new Text(160, 580, 'jump up here!', 10)
+    const yay = new Text(170, 550, 'nice', 20)
+    const wow = new Text(175, 480, 'wow', 15)
+
    arrowKeys.append()
    jump.append()
- 
+   jumpHere.append()
+   if(guy.position.x >= 135 && guy.position.x <= 225 && guy.position.y + guy.height >= 599 && guy.position.y + guy.height  <= 601){
+    yay.append()
+   } else if(guy.position.x >= 135 && guy.position.x <= 225 && guy.position.y + guy.height >= 500 && guy.position.y + guy.height  <= 540) {
+    wow.append()
+    }
 }
 // make keys array to look at for keypresses
 const keys = {
@@ -104,30 +135,46 @@ const keys = {
     }
 }
 
+function collision() {
+    guy.velocity.x = 0
+    if (keys.d.pressed) {guy.velocity.x = 4}
+    else if (keys.a.pressed) {guy.velocity.x = -4}
+    
+    if(guy.position.x >= 135 && guy.position.x <= 225 && guy.position.y + guy.height + guy.velocity.y  >= 600 && guy.position.y + guy.height <= 601 && guy.velocity.y !== 0){
+        guy.velocity.y = 0
+    } 
 
+}
+function crowTalk() {
+    c.clearRect(0, 0, canvas.width, canvas.height)
+}
 
 // the game function that will run when start button is pressed
 function game() {
-    // clear out the start screen
     c.clearRect(0, 0, canvas.width, canvas.height)
+    tree.append()
+    crow.append()
+    plat.append()
+// basic movement and spawning guy
+    instructions()
+    guy.move()
+
+    // action
+    if(guy.position.x > 360) {
+        setInterval(crowTalk(), 10000)
+    }
+    //spawn guy
+    // make first screen
+    
+
     // idk what this does completely but it makes it work
     //like animates it
     window.requestAnimationFrame(game)
-    plat.append()
-    //spawn guy
-    guy.move()
+    
     //set guy velocity
-    guy.velocity.x = 0
-    if (keys.d.pressed) {guy.velocity.x = 4}
-        else if (keys.a.pressed) {guy.velocity.x = -4}
-    instructions()
-// somehow made collision
-    if(guy.position.x >= 100 && guy.position.x <= 170 && guy.position.y + guy.height + guy.velocity.y  >= 600 && guy.position.y + guy.height <= 601 && guy.velocity.y !== 0){
-        // guy.position.y = 580
-        guy.velocity.y = 0
-    } else {
-        gravity = 0.6
-    }
+    // somehow made collision
+    collision()
+    console.log(guy.position.x)
 }
 
 game()

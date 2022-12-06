4ad9cf46b0fe967b3a43741b29d1f53010524970
tbfrobinson <tbfrobinson@gmail.com>
2022-12-05 17:09:24 -0800

    thought out hwo to make level 2

--- js/index.js
+++ js/index.js
@@ -17,22 +17,29 @@ let mainMenu = document.querySelector('#mainMenu')
 let restart = document.querySelector('#restart')
 
 
+let background
 
 let gravity = 0.6
 
-
-//character creation
+let level = 1
+if (level === 1) {
+    background = '/img/screenOne.png'
+}
+if (level === 2) {
+    background = '/img/guyBackwards.png'
+}
+// set background
 const screenOne = new Sprite({
     position: {
         x: 0,
         y: 0
     },
-    imageSrc: './img/screenOne.png'
+    imageSrc: background
 })
 // create character
 const guy = new Character({position: {
     x: 1,
-    y: 410,
+    y: 610,
 }, imageSrc: 'img/guy.png',
 frameRate: 3
 })
@@ -40,8 +47,12 @@ frameRate: 3
 function main() {
     location.reload()
 }
-
-
+function restartGame() {
+    level = 1
+    mainMenu.style.display = 'none'
+    restart.style.display = 'none'
+    guy.position.x = 1
+}
 function gameOver() {
     keys.d.pressed = false
     keys.a.pressed = false
@@ -52,19 +63,21 @@ function gameOver() {
     gameOver.append()
     mainMenu.style.display = 'block'
     restart.style.display = 'block'
-    // restart.addEventListener('click', restartGame)
-    // mainMenu.addEventListener('click', main)
+    restart.addEventListener('click', restartGame)
+    mainMenu.addEventListener('click', main)
 }
 
 const crowHello = new Text(433, 400, 'hey you, come closer', '12px Arial')
 const crowDetails = new Text(415, 390, 'I saw some guy go up this mountain with your head', '11px Arial')
 const crowWarn = new Text(433, 400, "don't die to that spike by the way", '12px Arial')
+const instructions = new Text(60, 170, 'walk around with the arrow keys', '40px Arial')
+const instructionsTwo = new Text(180, 220, 'jump with z!', '30px Arial')
+
 
 // the game function that will run when start button is pressed
 function game() {
     window.requestAnimationFrame(game)
     c.clearRect(0, 0, canvas.width, canvas.height)
-
     screenOne.draw()
     guy.move()
     // action
@@ -78,20 +91,30 @@ function game() {
     if (keys.d.pressed) {guy.velocity.x = 4}
     else if (keys.a.pressed) {guy.velocity.x = -4}
     
+    if (background == '/img/screenOne.png') {
+    if (guy.position.x < 140) {
+        instructions.append()
+        instructionsTwo.append()
+    }
     if (guy.position.x > 200 && guy.position.x < 320) {
         crowHello.append()
         console.log('crow hello')
-    } else if (guy.position.x > 370 && guy.position.x < 440) {
+    } else if (guy.position.x > 370 && guy.position.x < 480) {
         crowDetails.append()
         console.log('crow go up mountain')
     } else if (guy.position.x > 600) {
         crowWarn.append()
     }
 
-    if (guy.position.x >= 521 && guy.position.x <= 593 && guy.position.y >= 530) {
+    if (guy.position.x >= 525 && guy.position.x <= 593 && guy.position.y >= 530) {
         console.log('gameover')
         gameOver()
     }
-    console.log(guy.position.x, guy.position.y)
+    if(guy.position.x > 670){
+        level = 2
+    }
+    console.log(level, screenOne)
+}
+    
 }
 game()
